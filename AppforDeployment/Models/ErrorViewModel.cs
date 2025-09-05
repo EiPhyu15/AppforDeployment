4c30322dba397582a0b6081c713b95dd15331fec
EiPhyu15 <thineiphyu1561998@gmail.com>
2025-09-05 10:33:58 +0800

    InitialCreate

---
+++ AppforDeployment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppforDeployment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
